b7b9850f0d07faf81d4ca9f7700fe416866946c5
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2022-02-16 16:12:56 +0000

    Update joining_strings.py
    using .split to clean up the spaces, also using a for loop and .append

--- joining_strings.py
+++ joining_strings.py
@@ -11,3 +11,17 @@ print(limelight_lyrics_csv)
 # Join using \n as the delimiter. Prints as a paragraph, on separate lines.
 limelight_lyrcs = '\n'.join(limelight_first_verse)
 print(limelight_lyrcs)
+
+# .strip cleans up the data!
+# I'm using a for loop to iterate through the list
+# then using .append() to add the stripped lines to a new list.
+limelight_second_verse = ['Cast in this unlikely role, ill-equipped to act            ', 
+'With insufficient tact           ', 
+'            One must put up barriers to keep oneself intact.']
+
+limelight_second_verse_stripped = []
+for line in limelight_second_verse:
+    limelight_second_verse_stripped.append(line.strip())
+
+limelight_sv_full = '\n'.join(limelight_second_verse_stripped)
+print(limelight_sv_full)
